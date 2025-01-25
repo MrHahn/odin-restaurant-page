dcc7d4276a6d74dfd18d2ec1777bbeedadee4eba
Christopher Hahn <chahn970@gmail.com>
2025-01-24 17:58:59 -0700

    add click events and triggered functions to nav buttons

--- src/build.js
+++ src/build.js
@@ -1,13 +1,6 @@
 const wrapper = document.querySelector('#content');
 
-export const buildHero = () => {
-    const hero = document.createElement('div');
-    hero.classList.add('page-hero');
-    hero.innerHTML = `<h1>Odin's Seafood</h1>`;
-    wrapper.appendChild(hero);
-}
-
-export const buildAbout = () => {
+export const buildHome = () => {
     const about = document.createElement('div');
     about.classList.add('about');
     about.innerHTML = `<h3>We has the best Seafood in all of the places that don't have water.</h3>`;
--- src/index.js
+++ src/index.js
@@ -1,9 +1,40 @@
 import './styles.css';
-import {buildHero, buildAbout} from './build.js';
+import {buildHome} from './build.js';
 import {buildMenu} from './menu.js';
 import {buildContact} from './contact.js';
 
-// Build home page
-buildHero();
-buildAbout();
+const heroWrapper = document.querySelector('.hero');
+const wrapper = document.querySelector('#content');
+const homeBtn = document.querySelector('.home-btn');
+const menuBtn = document.querySelector('.menu-btn');
+const contactBtn = document.querySelector('.contact-btn');
 
+(function(){
+    const hero = document.createElement('div');
+    hero.classList.add('page-hero');
+    hero.innerHTML = `<h1>Odin's Seafood</h1>`;
+    heroWrapper.appendChild(hero);
+})();
+
+
+// Build home page
+buildHome();
+
+function clearDiv(){
+    wrapper.innerHTML = '';
+}
+
+homeBtn.addEventListener('click', () => {
+    clearDiv();
+    buildHome();
+})
+
+menuBtn.addEventListener('click', () => {
+    clearDiv();
+    buildMenu();
+})
+
+contactBtn.addEventListener('click', () => {
+    clearDiv();
+    buildContact();
+})
